98a10d868bfda2dcb501a7e93fe8a6a25c88d98f
ProjectGrantwood <justinphillipturner@gmail.com>
2023-03-29 15:02:00 -0400

    added test cases, formatted terminal output

--- Week-03-Arrays_and_Methods/CodingAssignmentWeek0304/CodingAssignmentWeek0304/src/AssignmentWeek0304.java
+++ Week-03-Arrays_and_Methods/CodingAssignmentWeek0304/CodingAssignmentWeek0304/src/AssignmentWeek0304.java
@@ -2,20 +2,26 @@
 
 public class AssignmentWeek0304 {
     public static void main(String[] args) {
-                
+        
+        lineBreak();
+        System.out.println("SOLUTIONS TO CODING ASSIGNMENT FOR WEEKS 03-04:");
+        
         // Create an array of int called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
         
         int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
         
         // Programmatically subtract the value of the first element in the array from the value in the last element of the array (i.e. do not use ages[7] in your code). 
         // Print the result to the console. 
-    
+        
+        lineBreak();
         System.out.println("The difference between " + ages[ages.length - 1] + " and " + ages[0] + " is " + (ages[ages.length - 1] - ages[0]) + ".");
     
         
         // Problem 3: Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
+        
         int[] ages2 = {3, 9, 23, 64, 2, 8, 28, 93, 72}; //added 72
         
+        lineBreak();
         System.out.println("The difference between " + ages2[ages2.length - 1] + " and " + ages2[0] + " is " + (ages2[ages2.length - 1] - ages2[0]) + ".");
         
         // Use a loop to iterate through the array and calculate the average age. Print the result to the console.
@@ -26,6 +32,7 @@ public class AssignmentWeek0304 {
             total += age;
         }
         
+        lineBreak();
         System.out.println("The average value in the array 'ages' is " + total / ages.length + ".");
         
         // Create an array of String called names that contains the following values: “Sam”, “Tommy”, “Tim”, “Sally”, “Buck”, “Bob”.
@@ -40,6 +47,8 @@ public class AssignmentWeek0304 {
             total += name.length();
         }
         
+        
+        lineBreak();
         System.out.println("The average number of letters for a name in the array 'names' is " + total / names.length + ".");
         
         // Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
@@ -49,6 +58,7 @@ public class AssignmentWeek0304 {
             concatenatedNames += " " + names[i];
         }
         
+        lineBreak();
         System.out.println("Here is a concatenated String of names: " + concatenatedNames);
         
         // How do you access the last element of any array?
@@ -56,8 +66,10 @@ public class AssignmentWeek0304 {
         
         String[] questions = {"How do you access the last element of an array?", "How do you access the first element of an array?"};
         String[] answers = {"You access the element at index array.length - 1", "You access the element at index 0"};
+        lineBreak();
         System.out.println(questions[0]);
         System.out.println(answers[answers.length - 1]);
+        lineBreak();
         System.out.println(questions[questions.length - 1]);
         System.out.println(answers[0]);
         
@@ -77,10 +89,61 @@ public class AssignmentWeek0304 {
             nameTotal += len;
         }
         
+        lineBreak();
         System.out.println("The total number of characters in our array of names is " + nameTotal + ".");
+        
+        // Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times. 
+        // (i.e. if I pass in “Hello” and 3, I expect the method to return “HelloHelloHello”).
+        
+        lineBreak();
+        testRepeatWord("hello", 3);
+        testRepeatWord("genie", 6);
+        
+        // Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).
+        
+        lineBreak();
+        testFullName("Gordon", "Freeman");
+        testFullName("Valerie", "Frizzle");
+        
+        // Write a method that takes an array of int and returns true if the sum of all the ints in the array is greater than 100.
+        
+        int[] testArr1 = {1, 2, 3, 4, 5};
+        int[] testArr2 = {20, 40, 60};
+        
+        lineBreak();
+        testTotalGreaterThan100(testArr1);
+        testTotalGreaterThan100(testArr2);
+        
+         // Write a method that takes an array of double and returns the average of all the elements in the array.
+         
+         double[] testArr3 = {0d, 1d, 2d, 3d};
+         double[] testArr4 = {0.1, 1.2, 2.3, 3.4};
+         
+         lineBreak();
+         testAverageOfArray(testArr3);
+         lineBreak();
+         testAverageOfArray(testArr4);
+         
+         // Write a method that takes two arrays of double and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+         
+         lineBreak();
+         testCompareAverages(testArr3, testArr4);
+         lineBreak();
+         testCompareAverages(testArr4, testArr3);
+         
+         lineBreak();
+         testWillBuyDrink(true, 9.0);
+         lineBreak();
+         testWillBuyDrink(true, 11.0);
+         lineBreak();
+         testWillBuyDrink(false, 9.0);
+         lineBreak();
+         testWillBuyDrink(false, 11.0);
+         
+         
     }
         
-    // Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in “Hello” and 3, I expect the method to return “HelloHelloHello”).
+    
     
     public static String repeatWord(String word, int n) {
         String result = "";
@@ -90,13 +153,19 @@ public class AssignmentWeek0304 {
         return result;
     }
     
-    // Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).
+    public static void testRepeatWord(String word, int n) {
+        String result = repeatWord(word, n);
+        System.out.println("The word '" + word + "' repeated " + n + " times: " + result + ".");
+    }
     
     public static String fullName (String firstName, String lastName) {
         return firstName + " " + lastName;
     }
     
-    // Write a method that takes an array of int and returns true if the sum of all the ints in the array is greater than 100.
+    public static void testFullName(String firstName, String lastName) {
+        String name = fullName(firstName, lastName);
+        System.out.println(firstName + "'s full name is " + name + ".");
+    }
     
     public static boolean totalGreaterThan100(int[] values) {
         int total = 0;
@@ -106,7 +175,13 @@ public class AssignmentWeek0304 {
         return total > 100;
     }
     
-    // Write a method that takes an array of double and returns the average of all the elements in the array.
+    public static void testTotalGreaterThan100(int[] values) {
+        System.out.println("The sum of the values in this array:");
+        for (int val: values){
+            System.out.println(val);
+        }
+        System.out.println("...is " + (totalGreaterThan100(values) ? "greater than" : "not greater than") + " 100.");
+    }
     
     public static double averageOfArray(double[] vals) {
         double total = 0d;
@@ -116,7 +191,13 @@ public class AssignmentWeek0304 {
         return total / vals.length;
     }
     
-    // Write a method that takes two arrays of double and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+    public static void testAverageOfArray(double[] vals) {
+        System.out.println("The average of this array:");
+        for (double val: vals){
+            System.out.println(val);
+        }
+        System.out.println("...is " + averageOfArray(vals) + ".");
+    }
     
     public static boolean compareAverages(double[] vals1, double[] vals2) {
         double average1 = averageOfArray(vals1);
@@ -124,10 +205,30 @@ public class AssignmentWeek0304 {
         return average1 > average2;
     }
     
-    // Write a method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+    public static void testCompareAverages(double[] vals1, double[] vals2) {
+        System.out.println("The average of the first array is " + averageOfArray(vals1) + ". The average of the second array is " + averageOfArray(vals2) + ".");
+        System.out.println("The first array's average is " + (compareAverages(vals1, vals2) ? "" : "not ") + "greater than the average of the second.");
+    }
     
     public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
         return isHotOutside && moneyInPocket > 10.5;
+    }
+    
+    public static void testWillBuyDrink(boolean isHotOutside, double moneyInPocket) {
+        boolean boughtDrink = willBuyDrink(isHotOutside, moneyInPocket);
+        if (isHotOutside){
+            System.out.println("It's sweltering.");
+            if (boughtDrink) {
+                System.out.println("...Good thing I had enough money to buy that expensive smoothie.");
+            } else {
+                System.out.println("...I wish I had enough money for a drink!");
+            }
+        } else {
+            System.out.println("I'll wait for a hot day to buy an expensive smoothie.");
+            if (!willBuyDrink(!isHotOutside, moneyInPocket)) {
+                System.out.println("...I'm gonna have to bring more money than I have in my wallet today, though.");
+            }
+        }
     }
     
     // Create a method of your own that solves a problem. In comments, write what the method does and why you created it.
@@ -170,4 +271,7 @@ public class AssignmentWeek0304 {
         return billTotalsWithTip;
     }
     
+    public static void lineBreak(){
+        System.out.println("");
+    }
 }
